a41b674be749771af5e5525e917f921b0463b20b
Steven Martinez <esteven123123@outlook.com>
2023-09-27 10:55:44 -0400

    create Course UI

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Layout, Compass } from "lucide-react";
+import { Layout, Compass, BarChart, List } from "lucide-react";
 import { SideBarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   {
@@ -16,8 +17,23 @@ const guestRoutes = [
   },
 ];
 
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+]
 export const SideBarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+  
 
   return (
     <div className="flex flex-col w-full">
